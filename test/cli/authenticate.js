c07b0620e9c0a20260d5b0a1afc900b42d5f37dd
Michael Fischer <michaelandrewfischer@gmail.com>
2021-02-28 21:42:03 -0600

    cli 100% coverage

--- test/cli/authenticate.js
+++ test/cli/authenticate.js
@@ -12,23 +12,51 @@ describe('CLI Test Suite', function () {
     return await generateTokens();
   };
 
-  let sandbox, authenticateSpy, initSpy, generateTokensSpy;
+  let sandbox, authenticateSpy, initSpy, generateTokensSpy, exitStub, logStub;
 
   before(() => {
     sandbox = sinon.createSandbox();
     authenticateSpy = sandbox.spy(authenticate);
     initSpy = sandbox.spy(init);
     generateTokensSpy = sandbox.spy(generateTokens);
+    exitStub = sandbox.stub(process, 'exit');
+    logStub = sandbox.stub(console, 'log');
   });
 
   beforeEach(() => {
     authenticateSpy.resetHistory();
     initSpy.resetHistory();
     generateTokensSpy.resetHistory();
+    exitStub.resetHistory();
+    logStub.resetHistory();
   });
 
   after(() => {
     sandbox.restore();
+  });
+
+  it('should abort and exit', (done) => {
+
+    proxyquire('../../cli/authenticate', {
+      'yargs/yargs': () => ({
+        argv: {
+          CONSUMER_KEY: undefined
+        }
+      }),
+      'yargs/helpers': {
+        hideBin() {}
+      },
+      '../lib/authenticate': {
+        init: initSpy,
+        generateTokens: generateTokensSpy,
+        authenticate: authenticateSpy
+      }
+    });
+
+    expect(exitStub.calledOnce).to.equal(true);
+    expect(logStub.calledOnce).to.equal(true);
+    done();
+    
   });
 
   it('should authenticate and return a token', (done) => {
@@ -53,8 +81,9 @@ describe('CLI Test Suite', function () {
     // wait until authenticate promise is resolved
     setTimeout(() => {
       expect(generateTokensSpy.called).to.equal(true);
+      expect(logStub.calledTwice).to.equal(true);
       done();
-    }, 50);
+    }, 20);
     
   });
 });
