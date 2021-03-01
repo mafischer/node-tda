4526ff90c9c4340626787f5bf45079680e2b2bf9
Michael Fischer <michaelandrewfischer@gmail.com>
2021-02-28 18:16:41 -0600

    file structure mirrors api docs

--- cli/authenticate.js
+++ cli/authenticate.js
@@ -2,7 +2,7 @@
 'use strict';
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
-const { authenticate, generateTokens } = require('../lib/auth');
+const { authenticate, generateTokens } = require('../lib/authenticate');
 
 const argv = yargs(hideBin(process.argv)).argv
 
