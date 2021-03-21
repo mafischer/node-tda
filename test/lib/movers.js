9d079160cf098afbf04d729c5a2debcba5d0f45f
Michael Fischer <michaelandrewfischer@gmail.com>
2021-03-20 21:35:04 -0500

    add movers api's

--- test/lib/movers.js
+++ test/lib/movers.js
@@ -1,10 +1,135 @@
 const { expect } = require('chai');
-const movers = require('../../lib/movers')
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+const movers = require('../../lib/movers');
 
 describe('Movers Test Suite', () => {
+  
+  let sandbox, axiosSuccess, axiosNotFound, axiosFailure;
 
-  it('should throw not implemented for getMovers', () => {
-    expect(movers.getMovers.bind()).to.throw();
+  before(() => {
+    sandbox = sinon.createSandbox();
+    axiosSuccess = sandbox.stub().resolves({
+      pizza: 'pepperoni'
+    });
+    axiosNotFound = sandbox.stub().rejects(new Error('404: Account not found!'));
+    axiosFailure = sandbox.stub().rejects(new Error('500: oops!'));
   });
 
+  beforeEach(() => {
+    axiosFailure.resetHistory();
+    axiosNotFound.resetHistory();
+    axiosSuccess.resetHistory();
+  });
+
+  after(() => {
+    sandbox.restore();
+  });
+
+  describe('getMovers Tests', () => {
+
+    it('should return data upon success', async () => {
+      const { getMovers } = proxyquire('../../lib/movers', {
+        '../helpers': {
+          axios: axiosSuccess
+        }
+      });
+      const response = await getMovers({
+        consumerKey: 'key',
+        index: '$DJI'
+      });
+      expect(response).to.deep.equal({
+        pizza: 'pepperoni'
+      });
+  
+    });
+
+    it('should return error upon 404', () => {
+      const { getMovers } = proxyquire('../../lib/movers', {
+        '../helpers': {
+          axios: axiosNotFound
+        }
+      });
+
+      const func = getMovers.bind({
+        token: 'testToken',
+        change: 'value'
+      });
+      expect(func).to.throw;
+  
+    });
+
+    it('should return error upon failure', (done) => {
+      const { getMovers } = proxyquire('../../lib/movers', {
+        '../helpers': {
+          axios: axiosFailure
+        }
+      });
+      getMovers({
+        token: 'testToken',
+        direction: 'up'
+      }).then(data => {
+        expect(data).to.be.undefined;
+        done();
+      }).catch(err => {
+        expect(err.message).to.equal('500: oops!');
+        done();
+      });
+  
+    });
+
+    it('should callback upon success', (done) => {
+      const { getMovers } = proxyquire('../../lib/movers', {
+        '../helpers': {
+          axios: axiosSuccess
+        }
+      });
+      getMovers({
+        token: 'testToken',
+        change: 'value'
+      }, (err, data) => {
+        expect(err).to.be.null;
+        expect(data).to.deep.equal({
+          pizza: 'pepperoni'
+        });
+        done();
+      });
+  
+    });
+
+    it('should callback upon 404', (done) => {
+      const { getMovers } = proxyquire('../../lib/movers', {
+        '../helpers': {
+          axios: axiosNotFound
+        }
+      });
+      getMovers({
+        token: 'testToken',
+        change: 'value'
+      }, (err, data) => {
+        expect(data).to.be.undefined;
+        expect(err.message).to.equal('404: Account not found!');
+        done();
+      });
+  
+    });
+
+    it('should callback upon failure', (done) => {
+      const { getMovers } = proxyquire('../../lib/movers', {
+        '../helpers': {
+          axios: axiosFailure
+        }
+      });
+      getMovers({
+        token: 'testToken',
+        change: 'value'
+      }, (err, data) => {
+        expect(data).to.be.undefined;
+        expect(err.message).to.equal('500: oops!');
+        done();
+      });
+  
+    });
+
+  });
 });
